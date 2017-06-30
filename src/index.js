3ef43ccd2f281774f8d8d921ddc454d8a2e0c220
Carlo Francisco <cfrancisco726@gmail.com>
2017-06-30 16:51:41 -0400

    section lecture 10 component instances

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+// importing from react module/library
+import ReactDom from 'react-dom'
 
-import App from './components/app';
-import reducers from './reducers';
+// create a new component. This component should produce some HTML.
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const App = function(){
+  return <div>Hi</div>;
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// const is es6 and declaring a variable
+// this is the final variable, its constant
+// JSX is a subset of Javascript represented in the div Hi
+// JSX gets turned into HTML. Purpose is to turn JS into HTML.
+// use babeljs compiler
+// JSX is supposed to make our code more ledgible
+
+// take this component's generated HTML and put it on the page (in the dom)
+
+ReactDom.render(App);
+// use react dom library when you want to render
+// need to instantiate components before we render to the DOM
+
+// everytime we write JSX
+// the component name is a component class but using inside JSX makes it a component instance
+
+<App></App>
+// a component instance
